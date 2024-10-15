b017c8eed7dd5057d1fbc4d916b24e36abb95e49
Sunaina04 <sunaina.takhi@genboot.com>
2024-10-15 16:57:58 +0530

    Section added for Customer Record

--- src/Components/Moormanage/Customer/Customer.tsx
+++ src/Components/Moormanage/Customer/Customer.tsx
@@ -474,7 +474,6 @@ const Customer = () => {
         style={{
           height: 'calc(100vh - 200px)',
           minHeight: 'calc(100vh - 200px)',
-          // minHeight: '700px',
           width: '500px',
           minWidth: '500px',
           backgroundColor: '#FFFFFF',
@@ -959,6 +958,7 @@ const Customer = () => {
               )}
 
               <div style={{ width: '450px' }} className="flex  flex-col wrapper">
+                {/* Mooring Section */}
                 <div
                   className=" relative  bg-white border-[1px] border-[#D5E1EA] mr-8"
                   style={{
@@ -997,7 +997,6 @@ const Customer = () => {
                             src="/assets/images/plus.png"
                             alt="Key Icon"
                             className="p-clickable"
-                            style={{}}
                           />
                         )}
                       </div>
@@ -1074,6 +1073,7 @@ const Customer = () => {
                     </div>
                   </div>
                 </div>
+                {/* Images Section */}
                 <div
                   className="tab relative bg-[#FFFFFF] border-[1px] border-[#D5E1EA] mr-8"
                   style={{
@@ -1090,6 +1090,334 @@ const Customer = () => {
                       <div style={{ flexShrink: 1 }}>
                         <h1 className="p-3 text-white text-lg font-extrabold">
                           {properties.imageHeader}
+                        </h1>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="p-2">
+                        {accordion === 'faq2' ? (
+                          <svg
+                            width="24"
+                            height="4"
+                            viewBox="0 0 11 3"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              d="M10.125 1.5C10.125 1.92188 9.77344 2.25 9.375 2.25H1.125C0.703125 2.25 0.375 1.92188 0.375 1.5C0.375 1.10156 0.703125 0.75 1.125 0.75H9.375C9.77344 0.75 10.125 1.10156 10.125 1.5Z"
+                              fill="white"
+                            />
+                          </svg>
+                        ) : (
+                          <img
+                            src="/assets/images/plus.png"
+                            alt="Key Icon"
+                            className="p-clickable"
+                            style={{}}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <div
+                    className={`content mt-5 transition-all ease-in-out duration-500 ${accordion === 'faq2' ? '' : 'hidden'}`}>
+                    <div
+                      className={`bg-#00426F overflow-x-hidden  table-container flex flex-col`}
+                      style={{ height: 'calc(100vh - 580px)' }}>
+                      <div className="flex-grow" style={{ overflow: 'auto' }}>
+                        <DataTableComponent
+                          style={{ borderBottom: '1px solid #D5E1EA', fontWeight: '400' }}
+                          scrollable
+                          tableStyle={{
+                            fontSize: '12px',
+                            color: '#000000',
+                            fontWeight: 600,
+                            backgroundColor: '#D9D9D9',
+                          }}
+                          data={customerImage}
+                          columns={customerImagesColumns}
+                          selectionMode="single"
+                          actionButtons={ActionButtonColumn}
+                          selection={selectedMooring}
+                          dataKey="id"
+                          rowStyle={(rowData: any) => rowData}
+                          emptyMessage={
+                            <div className="text-center mt-10">
+                              <img
+                                src="/assets/images/empty.png"
+                                alt="Empty Data"
+                                className="w-20 mx-auto mb-2"
+                              />
+                              <p className="text-gray-500 text-lg">{properties.noDataMessage}</p>
+                            </div>
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* WorkOrder Section */}
+                <div
+                  className="tab relative bg-[#FFFFFF] border-[1px] border-[#D5E1EA] mr-8"
+                  style={{
+                    width: '450px',
+                    maxWidth: '450px',
+                    marginTop: '0px',
+                  }}>
+                  <label
+                    htmlFor="faq2"
+                    style={{ backgroundColor: '#10293A' }}
+                    className="cursor-pointer flex items-center justify-between h-14"
+                    onClick={() => handleToggle('faq2')}>
+                    <div className="flex items-center">
+                      <div style={{ flexShrink: 1 }}>
+                        <h1 className="p-3 text-white text-lg font-extrabold">
+                          {properties.workOrderHeader}
+                        </h1>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="p-2">
+                        {accordion === 'faq2' ? (
+                          <svg
+                            width="24"
+                            height="4"
+                            viewBox="0 0 11 3"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              d="M10.125 1.5C10.125 1.92188 9.77344 2.25 9.375 2.25H1.125C0.703125 2.25 0.375 1.92188 0.375 1.5C0.375 1.10156 0.703125 0.75 1.125 0.75H9.375C9.77344 0.75 10.125 1.10156 10.125 1.5Z"
+                              fill="white"
+                            />
+                          </svg>
+                        ) : (
+                          <img
+                            src="/assets/images/plus.png"
+                            alt="Key Icon"
+                            className="p-clickable"
+                            style={{}}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <div
+                    className={`content mt-5 transition-all ease-in-out duration-500 ${accordion === 'faq2' ? '' : 'hidden'}`}>
+                    <div
+                      className={`bg-#00426F overflow-x-hidden  table-container flex flex-col`}
+                      style={{ height: 'calc(100vh - 580px)' }}>
+                      <div className="flex-grow" style={{ overflow: 'auto' }}>
+                        <DataTableComponent
+                          style={{ borderBottom: '1px solid #D5E1EA', fontWeight: '400' }}
+                          scrollable
+                          tableStyle={{
+                            fontSize: '12px',
+                            color: '#000000',
+                            fontWeight: 600,
+                            backgroundColor: '#D9D9D9',
+                          }}
+                          data={customerImage}
+                          columns={customerImagesColumns}
+                          selectionMode="single"
+                          actionButtons={ActionButtonColumn}
+                          selection={selectedMooring}
+                          dataKey="id"
+                          rowStyle={(rowData: any) => rowData}
+                          emptyMessage={
+                            <div className="text-center mt-10">
+                              <img
+                                src="/assets/images/empty.png"
+                                alt="Empty Data"
+                                className="w-20 mx-auto mb-2"
+                              />
+                              <p className="text-gray-500 text-lg">{properties.noDataMessage}</p>
+                            </div>
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Estimates Section */}
+                <div
+                  className="tab relative bg-[#FFFFFF] border-[1px] border-[#D5E1EA] mr-8"
+                  style={{
+                    width: '450px',
+                    maxWidth: '450px',
+                    marginTop: '0px',
+                  }}>
+                  <label
+                    htmlFor="faq2"
+                    style={{ backgroundColor: '#10293A' }}
+                    className="cursor-pointer flex items-center justify-between h-14"
+                    onClick={() => handleToggle('faq2')}>
+                    <div className="flex items-center">
+                      <div style={{ flexShrink: 1 }}>
+                        <h1 className="p-3 text-white text-lg font-extrabold">
+                          {properties.estimateHeader}
+                        </h1>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="p-2">
+                        {accordion === 'faq2' ? (
+                          <svg
+                            width="24"
+                            height="4"
+                            viewBox="0 0 11 3"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              d="M10.125 1.5C10.125 1.92188 9.77344 2.25 9.375 2.25H1.125C0.703125 2.25 0.375 1.92188 0.375 1.5C0.375 1.10156 0.703125 0.75 1.125 0.75H9.375C9.77344 0.75 10.125 1.10156 10.125 1.5Z"
+                              fill="white"
+                            />
+                          </svg>
+                        ) : (
+                          <img
+                            src="/assets/images/plus.png"
+                            alt="Key Icon"
+                            className="p-clickable"
+                            style={{}}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <div
+                    className={`content mt-5 transition-all ease-in-out duration-500 ${accordion === 'faq2' ? '' : 'hidden'}`}>
+                    <div
+                      className={`bg-#00426F overflow-x-hidden  table-container flex flex-col`}
+                      style={{ height: 'calc(100vh - 580px)' }}>
+                      <div className="flex-grow" style={{ overflow: 'auto' }}>
+                        <DataTableComponent
+                          style={{ borderBottom: '1px solid #D5E1EA', fontWeight: '400' }}
+                          scrollable
+                          tableStyle={{
+                            fontSize: '12px',
+                            color: '#000000',
+                            fontWeight: 600,
+                            backgroundColor: '#D9D9D9',
+                          }}
+                          data={customerImage}
+                          columns={customerImagesColumns}
+                          selectionMode="single"
+                          actionButtons={ActionButtonColumn}
+                          selection={selectedMooring}
+                          dataKey="id"
+                          rowStyle={(rowData: any) => rowData}
+                          emptyMessage={
+                            <div className="text-center mt-10">
+                              <img
+                                src="/assets/images/empty.png"
+                                alt="Empty Data"
+                                className="w-20 mx-auto mb-2"
+                              />
+                              <p className="text-gray-500 text-lg">{properties.noDataMessage}</p>
+                            </div>
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Billing Section */}
+                <div
+                  className="tab relative bg-[#FFFFFF] border-[1px] border-[#D5E1EA] mr-8"
+                  style={{
+                    width: '450px',
+                    maxWidth: '450px',
+                    marginTop: '0px',
+                  }}>
+                  <label
+                    htmlFor="faq2"
+                    style={{ backgroundColor: '#10293A' }}
+                    className="cursor-pointer flex items-center justify-between h-14"
+                    onClick={() => handleToggle('faq2')}>
+                    <div className="flex items-center">
+                      <div style={{ flexShrink: 1 }}>
+                        <h1 className="p-3 text-white text-lg font-extrabold">
+                          {properties.billingHeader}
+                        </h1>
+                      </div>
+                    </div>
+                    <div>
+                      <div className="p-2">
+                        {accordion === 'faq2' ? (
+                          <svg
+                            width="24"
+                            height="4"
+                            viewBox="0 0 11 3"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                              d="M10.125 1.5C10.125 1.92188 9.77344 2.25 9.375 2.25H1.125C0.703125 2.25 0.375 1.92188 0.375 1.5C0.375 1.10156 0.703125 0.75 1.125 0.75H9.375C9.77344 0.75 10.125 1.10156 10.125 1.5Z"
+                              fill="white"
+                            />
+                          </svg>
+                        ) : (
+                          <img
+                            src="/assets/images/plus.png"
+                            alt="Key Icon"
+                            className="p-clickable"
+                            style={{}}
+                          />
+                        )}
+                      </div>
+                    </div>
+                  </label>
+                  <div
+                    className={`content mt-5 transition-all ease-in-out duration-500 ${accordion === 'faq2' ? '' : 'hidden'}`}>
+                    <div
+                      className={`bg-#00426F overflow-x-hidden  table-container flex flex-col`}
+                      style={{ height: 'calc(100vh - 580px)' }}>
+                      <div className="flex-grow" style={{ overflow: 'auto' }}>
+                        <DataTableComponent
+                          style={{ borderBottom: '1px solid #D5E1EA', fontWeight: '400' }}
+                          scrollable
+                          tableStyle={{
+                            fontSize: '12px',
+                            color: '#000000',
+                            fontWeight: 600,
+                            backgroundColor: '#D9D9D9',
+                          }}
+                          data={customerImage}
+                          columns={customerImagesColumns}
+                          selectionMode="single"
+                          actionButtons={ActionButtonColumn}
+                          selection={selectedMooring}
+                          dataKey="id"
+                          rowStyle={(rowData: any) => rowData}
+                          emptyMessage={
+                            <div className="text-center mt-10">
+                              <img
+                                src="/assets/images/empty.png"
+                                alt="Empty Data"
+                                className="w-20 mx-auto mb-2"
+                              />
+                              <p className="text-gray-500 text-lg">{properties.noDataMessage}</p>
+                            </div>
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* Notes Section */}
+                <div
+                  className="tab relative bg-[#FFFFFF] border-[1px] border-[#D5E1EA] mr-8"
+                  style={{
+                    width: '450px',
+                    maxWidth: '450px',
+                    marginTop: '0px',
+                  }}>
+                  <label
+                    htmlFor="faq2"
+                    style={{ backgroundColor: '#10293A' }}
+                    className="cursor-pointer flex items-center justify-between h-14"
+                    onClick={() => handleToggle('faq2')}>
+                    <div className="flex items-center">
+                      <div style={{ flexShrink: 1 }}>
+                        <h1 className="p-3 text-white text-lg font-extrabold">
+                          {properties.notesHeader}
                         </h1>
                       </div>
                     </div>
