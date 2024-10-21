f1f4b1ab40e2abc669e666bc99d4e8b816901844
Sunaina04 <sunaina.takhi@genboot.com>
2024-10-21 19:30:35 +0530

    Voice Memo Updated Done

--- src/Components/CommonComponent/VoiceMemoPlayer.tsx
+++ src/Components/CommonComponent/VoiceMemoPlayer.tsx
@@ -10,15 +10,14 @@ const VoiceMemoPlayer: React.FC<PlayVoiceMemoProps> = ({
   const audioSrc = `data:audio/aac;base64,${voiceMemo?.encodedData}`
 
   return (
-    <div>
-      <h2>{voiceMemo?.name}</h2>
-      <Dialog visible={imageVisible} onHide={() => setImageVisible(false)}>
-        <h2>{voiceMemo?.name}</h2>
+    <div className="flex items-center justify-center h-20 mt-20">
+      <div className="text-center">
+        <h2 className="mb-4">{voiceMemo?.name}</h2>
         <audio controls>
           <source src={audioSrc} type="audio/aac" />
           Your browser does not support the audio tag.
         </audio>
-      </Dialog>
+      </div>
     </div>
   )
 }
